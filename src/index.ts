71a1f4f2ef3fafbb439f3f5053feea8da482117c
Aleksandar Gekov <alexander.gekov00@gmail.com>
2024-10-05 14:06:37 +0300

    load docs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { JSONLoader } from "langchain/document_loaders/fs/json";
 
-const loader = new JSONLoader("src/document_loaders/example_data/example.json");
+const loader = new JSONLoader("data/session.json");
 
 const docs = await loader.load();
+
+console.log(docs);
